f73f8aea3769109f206760e554eda911facd32bf
Luda68 <pril3pskay@yandex.ru>
2024-05-17 16:04:38 +0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,17 @@
+x=38
+print("Дратути")
+if x<0:
+    print("Меньше нуля")
+print("Дотвидания!")
+
+a, b=10, 5
+if (a>b):
+    print("a>b")
+if (a>b and a>0):
+    print("OK")
+if (a>b) and (a>0 or b<1000):
+    print("OK")
+if 5<b and b<10:
+    print("OK")
+
+
